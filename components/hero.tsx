6006cd87905995df5fff37c7563372d885d55ec3
RBC\220662279 <Lucas.Winkler@regalrexnord.com>
2024-08-25 15:14:02 -0500

    hero button styling fixes

--- components/hero.tsx
+++ components/hero.tsx
@@ -116,7 +116,7 @@ const Hero = () => {
               <p className="text-xl">{desc}</p>
               <Link
                 href={url}
-                className="py-2 px-4 rounded bg-slate-500 w-fit shadow-lg"
+                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-fit"
               >
                 Find Out More
               </Link>
