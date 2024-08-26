276964f87e108f2abfcdb090ee1f0aedacf004da
RBC\220662279 <Lucas.Winkler@regalrexnord.com>
2024-08-25 21:18:23 -0500

    new headshots

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
 import Hero from "@/components/hero";
 import Image from "next/image";
-import headshot from "@/public/headshot.webp";
+import headshot from "@/public/headshot1.jpg";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col gap-6">
-      <section className="container flex flex-col lg:flex-row lg:justify-between my-16">
-        <div className="flex flex-col">
-          <h2 className="text-xl font-bold tracking-widest">ABOUT</h2>
-          <Image
-            height={300}
-            width={300}
-            alt="Headshot of Justice Voss."
-            src={headshot}
-            className="-mt-8 h-auto"
-            priority
-          />
+      <section className="container flex flex-col lg:flex-row lg:gap-24 mb-24">
+        <div className="flex flex-col mt-8">
+          <h2 className="text-2xl font-bold tracking-widest mt-8 mb-4">ABOUT</h2>
+          <div className="rounded-full overflow-hidden aspect-square">
+            <Image
+              height={300}
+              width={300}
+              alt="Headshot of Justice Voss."
+              src={headshot}
+              priority
+            />
+          </div>
         </div>
-        <p className="lg:w-1/2 mt-6 lg:mt-24">
+        <p className="lg:w-1/2 mt-6 lg:mt-24 lg:text-xl lg:font-semibold">
           I am a recent game design graduate from Full Sail University.
           <br />
           <br />
